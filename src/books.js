0f4577edc3bf0322a4d1847d0224eb0163f311d7
mgary5979 <mgary5979@gmail.com>
2023-03-20 09:20:25 +0000

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "Benjamin Hall",
+    title: "Saved: A War Reporter's Mission To Make It Home.",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Adam Wallace",
+    title: "How to catch a Leprechaun.",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Ann Napolitano",
+    title: "Hello Beautiful",
+    img: img3,
+    id: 3,
+  },
+];
